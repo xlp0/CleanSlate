e19beaeab2ce2a0e73c73c52e4c939724cc4fa1a
Ben Koo <koo0905@gmail.com>
2021-06-20 15:20:46 +0800

    removed dos2unix and changed the gpg instruction to have default -y option turned on

--- setupDockerAndK8s.sh
+++ setupDockerAndK8s.sh
@@ -12,12 +12,9 @@ apt-get install -y \
     lsb-release \
     zip \
     unzip \
-    cron \
-    dos2unix
+    cron
 
-dos2unix *.sh
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
