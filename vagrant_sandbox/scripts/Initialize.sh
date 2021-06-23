b4d380adaea396836953cfb0a1212c1f019847ee
Ben Koo <koo0905@gmail.com>
2021-06-24 00:02:39 +0800

    Added some reference files

--- vagrant_sandbox/scripts/Initialize.sh
+++ vagrant_sandbox/scripts/Initialize.sh
@@ -5,7 +5,7 @@ add-apt-repository universe
 apt-get update
 
 # Download Github.com project xlp0/CleanSlate
-echo "Try to download Github/xlp0/CleanSlate project to the ../CleanSlate directory."
-git clone https://github.com/xlp0/CleanSlate ../CleanSlate
+#echo "Try to download Github/xlp0/CleanSlate project to the ../CleanSlate directory."
+#git clone https://github.com/xlp0/CleanSlate ../CleanSlate
 
-sudo swapoff -a
+# sudo swapoff -a
