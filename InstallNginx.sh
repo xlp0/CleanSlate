04a30824cf763098d01c14d2cf55594d22d3aca1
Ubuntu <ubuntu@ip-172-31-19-244.ec2.internal>
2021-07-04 12:54:20 +0000

    Added some files to accelerate installation process

---
+++ InstallNginx.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+apt-get update
+apt-get install nginx -y
+
+unlink /etc/nginx/sites-enabled/default
+
+cp ./resources/reverse-proxy.conf /etc/nginx/sites-available/.
+
+ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+
+add-apt-repository ppa:certbot/certbot
+
+apt install python3-certbot-nginx
+
+nginx -t
+
+systemctl reload nginx
