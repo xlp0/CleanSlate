6cbb67fba9008c7d0aa366713e5dd0361f5744e5
Muhammad Haviz <emhavis@gmail.com>
2021-07-15 19:31:08 +0700

    add localSettings.php configuration

--- kubernetes_deployment/up.sh
+++ kubernetes_deployment/up.sh
@@ -22,18 +22,22 @@ fi
 
 # Must mount from local host to kube
 echo "Mounting to: $LOCAL_FOLDER:/home/docker/mount"
-minikube start --mount --mount-string="$LOCAL_FOLDER:/home/docker/mount"
+# minikube start --mount --mount-string="$LOCAL_FOLDER:/home/docker/mount"
 
 # create execute yaml file, overwrite if exist
 cp clean_slate.yml clean_slate_exec.yml
 
 # Apply .env parameters into yaml file
 # amount of local storage
-sed -i -e 's, LOCAL_STORAGE_SPACE, '$LOCAL_STORAGE_SPACE',g' clean_slate_exec.yml
+# sed -i -e 's, LOCAL_STORAGE_SPACE, '$LOCAL_STORAGE_SPACE',g' clean_slate_exec.yml
 # External exposed IP address
-sed -i -e 's, EXTERNAL_IP_ADDRESS, '$EXTERNAL_IP_ADDRESS',g' clean_slate_exec.yml
+# sed -i -e 's, EXTERNAL_IP_ADDRESS, '$EXTERNAL_IP_ADDRESS',g' clean_slate_exec.yml
 # External exposed port number
-sed -i -e 's, EXPOSED_PORT_NUMBER, '$EXPOSED_PORT_NUMBER',g' clean_slate_exec.yml
+# sed -i -e 's, EXPOSED_PORT_NUMBER, '$EXPOSED_PORT_NUMBER',g' clean_slate_exec.yml
+
+# create execute yaml file, overwrite if exist
+cp $LOCAL_FOLDER/LocalSettings-template.php $LOCAL_FOLDER/LocalSettings.php
+sed -e 's, SERVER_URL, "'http://$EXTERNAL_IP_ADDRESS:$EXPOSED_PORT_NUMBER'",g' $LOCAL_FOLDER/LocalSettings.php > $LOCAL_FOLDER/tmp.php && mv $LOCAL_FOLDER/tmp.php $LOCAL_FOLDER/LocalSettings.php
 
 # apply config files
-kubectl apply -f clean_slate_exec.yml
+# kubectl apply -f clean_slate_exec.yml
