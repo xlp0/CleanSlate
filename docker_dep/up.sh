a34aa80720c8abb920dbf265e1be272e88ae608a
EC2 Default User <ec2-user@ip-172-31-3-47.us-west-2.compute.internal>
2021-09-19 03:19:27 +0000

    change up.sh

--- docker_dep/up.sh
+++ docker_dep/up.sh
@@ -7,7 +7,6 @@ if [ -f .env ]; then
     echo "Loaded environmental variable: PORT_NUMBER=$PORT_NUMBER"
 fi
 
-
 # In case, there is no .machine_specific_env file
 TRANSPORT_STRING="http"
 HOST_STRING="localhost"
@@ -23,7 +22,7 @@ fi
 # Localhost configuration based on .env
     # Substitute to correct config if founded "$TargetKey =" in LocalSetting.php
     # Only replace 'var' instead of "var" 
-    LOCALSETTINGS_FILENAME="./mountPoint/LocalSettings.php"
+    LOCALSETTINGS_FILENAME="./mountpoint/LocalSettings.php"
     # Put in all the params for configuration
     key_array=(
       "wgServer"
@@ -48,14 +47,21 @@ fi
 # read -p "Please review the parameter and Press any key to continue or ctrl-C to cancel"
 
 echo 'Preparing mountPoint folder'
-# tar -xzvf mountPoint.tar.gz
+tar -xzvf mountpoint.tar.gz
+
+docker-compose up -d
+echo 'Waits until all container is active'
+sleep 4  # Waits 5 seconds.
+# run this command after everything
+# php /var/www/html/maintenance/update.php --quick
+# docker exec -d xlp_mediawiki php /var/www/html/maintenance/update.php --quick 
+# docker exec -d xlp_mediawiki php /var/www/html/maintenance/rebuildrecentchanges.php
+# docker exec -d xlp_mediawiki php /var/www/html/maintenance/initSiteStats.php
+echo "Update database Media Wiki"
+docker exec -d xlp_mediawiki php /var/www/html/maintenance/update.php --quick 
+# ./update-sw.sh
 
 echo "Please go to a browser and use http://$HOST_STRING:$MATOMO_PORT_NUMBER to access Matomo" 
 echo "Please go to a browser and use http://$HOST_STRING:$PORT_NUMBER to test the WikiMedia"
-
-docker-compose up -d
-# run this command after everything
-# php /var/www/html/maintenance/update.php --quick
-docker exec -d xlp_mediawiki php /var/www/html/maintenance/update.php --quick 
-docker exec -d xlp_mediawiki php /var/www/html/maintenance/rebuildrecentchanges.php
-docker exec -d xlp_mediawiki php /var/www/html/maintenance/initSiteStats.php
+echo "Please go to a browser and use http://$HOST_STRING:$GITEA_PORT_NUMBER to test the Gitea"
+echo "Please go to a browser and use http://$HOST_STRING:$PHP_MA to test the PHPMyAdmin"
